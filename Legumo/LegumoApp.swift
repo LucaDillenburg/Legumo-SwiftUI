375229996f4088c0c78a41b5ff55f5ce629dfb6c
Luca Dillenburg <luca.assumpcao.dillenburg@gmail.com>
2021-11-16 19:57:09 -0300

    Initial Commit

---
+++ Legumo/LegumoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LegumoApp.swift
+//  Legumo
+//
+//  Created by Luca Dillenburg on 16/11/21.
+//
+
+import SwiftUI
+
+@main
+struct LegumoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
